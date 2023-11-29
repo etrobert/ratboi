112cf3b804f3b1782ed8b43200ffd91f67721bbb
erobert <etiennerobert33@gmail.com>
2023-11-29 07:42:02 +0100

    Add dev & prod envs

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
-fetch('http://localhost:54321/functions/v1/hello-world', {
+const dev = {
+  SUPABASE_URL: 'http://localhost:54321',
+  SUPABASE_ANON_KEY:
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS1kZW1vIiwicm9sZSI6ImFub24iLCJleHAiOjE5ODM4MTI5OTZ9.CRXP1A7WOeoJeXxjNni43kdQwgnWNReilDMblYTn_I0',
+};
+
+const prod = {
+  SUPABASE_URL: 'https://vyvqjhydaykkoatqohiu.supabase.co',
+  SUPABASE_ANON_KEY:
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZ5dnFqaHlkYXlra29hdHFvaGl1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDEyMzI4OTcsImV4cCI6MjAxNjgwODg5N30.N7ZTPZb-6LnyjcWJOzf1jibHYtZ9VoB-snxHJEt1DcQ',
+};
+
+const env = prod;
+
+fetch(`${env.SUPABASE_URL}/functions/v1/hello-world`, {
   method: 'POST',
   headers: {
-    Authorization:
-      'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS1kZW1vIiwicm9sZSI6ImFub24iLCJleHAiOjE5ODM4MTI5OTZ9.CRXP1A7WOeoJeXxjNni43kdQwgnWNReilDMblYTn_I0',
+    Authorization: `Bearer ${env.SUPABASE_ANON_KEY}`,
     'Content-Type': 'application/json',
   },
   body: JSON.stringify({
